320472ba1eb2ff453885fcaf2e91206763d32289
Lucas <joselucas0303@gmail.com>
2020-06-01 00:13:51 -0300

    support the odd and even numbers

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 from functions.parser_functions import *
+from functions.ui import *
 import sys
 
 def main():
     argc = len(sys.argv)
     values = sys.argv[1:]
-    commands = ('add', 'sub', 'mult', 'div', '-f')
+    commands = ('add', 'sub', 'mult', 'div')
+    subcommands = get_subcommands(values, ('-even', '-odd', '-f'), 2)
     
+    if subcommands == -1 or ('-even' in subcommands and '-odd' in subcommands):
+        print("Expression is wrong.")
+        print("Usage: python main.py [-h or --help]")
+        sys.exit(1)
+    
+    start = len(subcommands)
     if ('--help' in values or '-h' in values) and argc == 2:
-        print("This is PyCalc!")
-        print("Available commands:")
-        print("add - sum two values; x [add] y")
-        print("sub - subtract two values; x [sub] y")
-        print("multi - multiply two values; x [multi] y")
-        print("div - divide two values; x [div] y")
-        print("\nUse the '-f' flag to float numbers.")
-        print("\nExample: 10 add 60 div 2 add 2")
-        print("42")
+        show_doc()
         sys.exit(0)
     
     elif argc == 1:
         print("Usage: python main.py [-h or --help]")
-        sys.exit(1)
+        sys.exit(2)
 
-    elif not (check_commands(values, commands)) or not (check_expr(values)):
+    elif not (check_commands(values[start:], commands)) or not (check_expr(values[start:])):
         print("Expression is wrong.")
         print("Usage: python main.py [-h or --help]")
-        sys.exit(2)
+        sys.exit(3)
 
     sys.exit(0)
 
