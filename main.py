889f690f5a0cf45306b991a1d39df024cb87f7c6
Lucas <joselucas0303@gmail.com>
2020-06-01 14:21:47 -0300

    bugfix and the program is functional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions.parser_functions import check_commands, check_expr, get_flags
-from functions.ui import *
+from functions.calc_functions import calc_expr
+from functions.ui import show_doc
 import sys
 
 def main():
@@ -7,13 +8,14 @@ def main():
     values = sys.argv[1:]
     commands = ('add', 'sub', 'mult', 'div')
     flag = get_flags(values, ('-even', '-odd', '-f'), 1)
-    
+
     if flag == -1:
         print("Expression is wrong.")
         print("Usage: python main.py [-h or --help]")
         sys.exit(1)
     
-    start = len(flag)
+    start = len(flag) if flag[0] != None else 0
+
     if ('--help' in values or '-h' in values) and argc == 2:
         show_doc()
         sys.exit(0)
@@ -27,6 +29,10 @@ def main():
         print("Usage: python main.py [-h or --help]")
         sys.exit(3)
 
+    result = calc_expr(values[start:], True if flag[0] == '-f' else False)
+    result = int(result) if flag[0] != '-f' else result
+    print(result)
+    
     sys.exit(0)
 
 if __name__ == "__main__":
