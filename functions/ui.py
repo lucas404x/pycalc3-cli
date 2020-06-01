f9e113bb758ee2439ced83bb87e4ed30b1e877a1
Lucas <joselucas0303@gmail.com>
2020-06-01 00:15:21 -0300

    ui file and show doc function add in repository

---
+++ functions/ui.py
@@ -0,0 +1,11 @@
+def show_doc():
+    print("This is PyCalc!")
+    print("Available commands:")
+    print("add -> sum two values; x [add] y")
+    print("sub -> subtract two values; x [sub] y")
+    print("multi -> multiply two values; x [multi] y")
+    print("div -> divide two values; x [div] y")
+    print("\n-odd or -even -> the program will accept just odd or even numbers.")
+    print("-f: flag to float numbers.")
+    print("\nExample: 10 add 60 div 2 add 2")
+    print("42")
