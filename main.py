d9daf38902b8880a334379a86904f8dc75da64e7
Lucas <joselucas0303@gmail.com>
2020-06-03 16:20:05 -0300

    constant flag quantity created

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from functions.calc_functions import calc_expr
 from functions.ui import show_doc
 import sys
 
+FLAGS_QUANTITY_ALLOWED = 1
+
 def main():
     argc = len(sys.argv)
     values = sys.argv[1:]
     commands = ('add', 'sub', 'mult', 'div')
-    flag = get_flags(values, ('-even', '-odd', '-f'), 1)
+    flag = get_flags(values, ('-even', '-odd', '-f'), FLAGS_QUANTITY_ALLOWED)[0]
+    flag_index = FLAGS_QUANTITY_ALLOWED if flag != None else 0
 
     if flag == -1:
         print("Expression is wrong.")
         print("Usage: python main.py [-h or --help]")
         sys.exit(1)
-    
-    start = len(flag) if flag[0] != None else 0
 
     if ('--help' in values or '-h' in values) and argc == 2:
         show_doc()
@@ -24,13 +25,13 @@ def main():
         print("Usage: python main.py [-h or --help]")
         sys.exit(2)
 
-    elif not (check_commands(values[start:], commands)) or not (check_expr(values[start:], flag[0])):
+    elif not (check_commands(values[flag_index:], commands)) or not (check_expr(values[flag_index:], flag)):
         print("Expression is wrong.")
         print("Usage: python main.py [-h or --help]")
         sys.exit(3)
 
-    result = calc_expr(values[start:]) 
-    result = int(result) if flag[0] != '-f' else result
+    result = calc_expr(values[flag_index:]) 
+    result = int(result) if flag != '-f' else result
     print(result)
     
     sys.exit(0)
